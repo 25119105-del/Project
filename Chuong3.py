d2d4c2245e255d1fa9d521d111315ef4aff4ab4c
Nguyễn Vũ Tấn Khoa <25119105@gmail.hcmute.edu.vn>
2026-02-04 08:14:34 +0700

    đổi file mơi

--- Chuong3.py
+++ Chuong3.py
@@ -4,7 +4,21 @@
 # for i in range(0,6,2):
 #     print(i+1)
 
-for i in range(0,3):
-    print(i)
+# for i in range(0,3):
+#     print(i)
+#     for j in range(3,5):
+#         print(j)
+
+# i =0
+# for j in range(3,5):
+#     print(j)
+#     print(i)
+# i=1
+# for j in range(3,5):
+#     print(j)
+#     print(i)
+
+for i in range(0,2):
     for j in range(3,5):
         print(j)
+        print(i)
